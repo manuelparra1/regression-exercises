3528e1b4a91bde210c52fa93beea5e64446cfb3c
Manuel Parra <manuel.parra1@gmail.com>
2023-01-23 10:21:30 -0600

    refactor evaluate & wrangle

--- evaluate.py
+++ evaluate.py
@@ -1,12 +1,46 @@
+from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score
+import matplotlib.pyplot as plt
+
 def plot_residuals(y, yhat):
 
-def regression_errors(y, yhat):
-    sum of squared errors (SSE)
-    explained sum of squares (ESS)
-    total sum of squares (TSS)
-    mean squared error (MSE)
-    root mean squared error (RMSE)
+    # Calculate Residuals
+    residuals = y - yhat
+
+    # P L O T T I N G
+    plt.hlines(0, y.min(), y.max(), ls='--')
+    plt.scatter(y, residuals)
+    plt.ylabel('residual ($y - \hat{y}$)')
+    plt.xlabel('y value ($y$)')
+    plt.title('y vs yhat')
+    plt.show()
+
+def regression_errors(y, yhat): 
     
+    MSE = mean_squared_error(y, yhat)
+    SSE = MSE * len(y)
+    RMSE = MSE**.5
+    
+    ESS = ((yhat - y.mean())**2).sum()
+    TSS = ESS + SSE
+    
+    return SSE, ESS, TSS, MSE, RMSE
+
 def baseline_mean_errors(y):
+
+    baseline = np.repeat(y.mean(), len(y))
     
-def better_than_baseline(y):
+    MSE = mean_squared_error(y, baseline)
+    SSE = MSE * len(y)
+    RMSE = MSE**.5
+    
+    return SSE, MSE, RMSE   
+
+def better_than_baseline(y, yhat):
+    SSE, ESS, TSS, MSE, RMSE = regression_errors(y, yhat)
+    
+    SSE_baseline, MSE_baseline, RMSE_baseline = baseline_mean_errors(y)
+    
+    if SSE < SSE_baseline:
+        print('better than baseline')
+    else:
+        print('not better than baseline')
